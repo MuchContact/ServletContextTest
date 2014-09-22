37aa5eb43ad55d7a48cbe574665ed2ba8058ae39
MuchContact <MuchContact@users.noreply.github.com>
2014-09-22 19:45:36 +0800

    删除多余注释

--- src/zondy/com/MyServletContextListener.java
+++ src/zondy/com/MyServletContextListener.java
@@ -6,12 +6,10 @@ import javax.servlet.ServletContextListener;
 public class MyServletContextListener implements ServletContextListener {
 
 	public void contextDestroyed(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
 		System.out.println("ServletContext destroyed!");
 	}
 
 	public void contextInitialized(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
 		System.out.println("ServletContext created!");
 	}
 
